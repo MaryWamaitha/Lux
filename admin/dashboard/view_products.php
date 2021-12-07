207cea64fdffaee575af59c63e07d690bf682562
MaryWamaitha <mary.wamaitha@ashesi.edu.gh>
2021-12-07 13:17:54 +0000

    products

--- admin/dashboard/view_products.php
+++ admin/dashboard/view_products.php
@@ -60,7 +60,7 @@ $products = select_all_products_controller();
       </button>
       <!-- Brand -->
       <a class="navbar-brand pt-0" href="../index.html">
-        <img src="../assets/img/brand/blue.png" class="navbar-brand-img" alt="...">
+        <img src="../../images/logo5.png" class="navbar-brand-img" alt="...">
       </a>
       <!-- User -->
       <ul class="nav align-items-center d-md-none">
