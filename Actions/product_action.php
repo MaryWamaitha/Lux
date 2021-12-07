8bcbf535b48e86b664faa59e6b36da676d4038d2
MaryWamaitha <mary.wamaitha@ashesi.edu.gh>
2021-12-07 12:59:30 +0000

    products

--- Actions/product_action.php
+++ Actions/product_action.php
@@ -47,11 +47,12 @@ if(isset($_POST['saveproduct'])){
                             if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $target_file)) {
                                 $result = add_product_controller($category,$title,$price,$desc);
                                 if ($result=== true){
+                                    var_dump($result);
                                     $recent_product = get_last_product_controller();
                                     $image=$target_file;
                                     $add_image=add_image_controller($recent_product['currentProduct'],$image);
                                     if ($add_image===true){
-                                        header("Location: ../admin/dashboard/view_products.php");
+                                        var_dump( $add_image);
                                     } else {
                                         header("Location: ../admin/dashboard/add_products.php?error=5");
                                     }
